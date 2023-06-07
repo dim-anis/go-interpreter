e66b75c47571da520ff4651d946a64be4b5a8a26
dim-anis <anis.dim@gmail.com>
2023-06-07 19:41:10 +0400

    finish lexer, start building parser

--- src/main.go
+++ src/main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
-	// "go-interpreter/token"
+	"fmt"
+	"go-interpreter/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+  fmt.Printf("Hi, %s! This is the go interpreter!\n", user.Username)
+	fmt.Printf("Feel free to type something\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
